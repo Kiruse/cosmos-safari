b11179c0ccb608d6337435c0037bc54b16e7b8c5
kiruse <26940742+Kiruse@users.noreply.github.com>
2023-08-06 00:54:14 +0200

    change to checking for new props every 60m

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import { ExpoPushReporter } from './reporters/expo-push.js'
 import safari from './safari.js'
 import { getPropsTask } from './safari/props.js'
-import { daily } from './scheduler.js'
+import { interval } from './scheduler.js'
 import init from './init.js'
 
 safari.addReporter(new ExpoPushReporter(init.expo));
 
-// check the blockchain 3 times a day for new proposals
-daily('08:00:00', '14:00:00', '20:00:00')(getPropsTask, { runNow: true });
+// check the blockchain every hour for new proposals
+interval(3600, getPropsTask, { runNow: true });
